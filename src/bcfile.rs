6c1a68d80519b2a420ea2d8653277b691f6b1dd6
Olga Cosculluela Palacin <olgacoscu@gmail.com>
2022-05-13 09:16:57 +0200

    Ajout transactions.json to .gitignore et methods pour les transactions dans file

--- src/bcfile.rs
+++ src/bcfile.rs
@@ -97,3 +97,14 @@ pub fn store_version_message(target_address: &String, (_, _, _, _): (u32, Vec<u8
     // msg.push_str(format!("services = {:?}\n", services ).as_str());
     store_event(&msg);
 }
+pub fn store_transaction_message (transaction: &String){
+    store_event(&transaction);
+
+    let mut file = LineWriter::new(File::create("./transactions-found.json").unwrap());
+    file.write_all(b"[\n").unwrap();
+    file.write_all(transaction.as_bytes()).as_ref().unwrap();
+    file.write_all(b"]").unwrap();
+    drop(file);
+    fs::rename("./transactions-found.json", "./transactions.json").unwrap();
+
+}
